82003e563dac642d022b3ab826d933176253d3db
DiegoDominguez <ddominguez@stx.com.mx>
2021-06-22 16:56:12 -0500

    Avance outputs

--- src/app/pages/progress/progress.component.ts
+++ src/app/pages/progress/progress.component.ts
@@ -1,16 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-progress',
   templateUrl: './progress.component.html',
-  styles: [
-  ]
+  styleUrls: [ './progress.component.css' ]
 })
-export class ProgressComponent implements OnInit {
+export class ProgressComponent {
 
-  constructor() { }
+  public progreso1: number = 25;
+  public progreso2: number = 55;
 
-  ngOnInit(): void {
+
+  get getPorcentaje1() {
+    return `${ this.progreso1 }%`
+  }
+
+   get getPorcentaje2() {
+    return `${ this.progreso2 }%`
   }
 
 }
